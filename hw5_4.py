1150742d159be71cf32ae79d0a3be90acad39d6a
Oleksandr <sanelka77@yahoo.com>
2024-02-21 23:28:50 +0200

    universal decorator input_error

--- hw5_4.py
+++ hw5_4.py
@@ -1,42 +1,63 @@
+import re
+
+
 def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except ValueError as err:
-            return f'{err}'
-        except IndexError as err:
-            return f'{err}'
-        except KeyError as err:
-            return f'{err}'
-        except Exception as err:
-            return f'{err}'
+        except ValueError as error:
+            return f'{error}'
+        except IndexError as error:
+            return f'{error}'
+        except KeyError as error:
+            return f'{error}'
+        except Exception as error:
+            return f'{error}'
 
     return inner
 
 
 @input_error
 def parse_input(user_input):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
+# Function return a list with arguments or a error code.
+
+    if re.search(r'.*[" "].*[" "].*',  user_input):
+        cmd, *args = user_input.split()
+        cmd = cmd.strip().lower()
+    else:
+        return [-1]
+    commands = ('close', 'exit', 'hello', 'add', 'change', 'phone', 'all')
+    if not cmd in commands:
+        return [-2]
     return cmd, *args
+
 
 @input_error
 def add_contact(args, contacts):
     name, phone = args
-    if name in contacts:
-        raise ValueError ("The contact already exists. Addendum canceled.")
-    else:
-        contacts[name] = phone
-        return "Contact added."
+    phone = re.sub(r'[^*#pw0-9]', r'', phone)
+    phone = phone if phone[0] in '+*#pw' else f'+{phone}'
+
+    if not re.search(r'[0-9][0-9][0-9]', phone):
+        raise ValueError ('Too few digits in the number')
+    contacts[name] = phone
+    return 'Contact added.'
+
 
 @input_error
 def change_contact(args, contacts):
+# Updates an existing contact or adds a new one.
+    
     name, phone = args
     if name in contacts:
-        contacts[name] = phone
-        return "Contact updated."
+        result = add_contact(args, contacts)
+        if contacts[name] == phone:
+            return 'Contact updated.'
+        else:
+            return result
     else:
-        raise ValueError(f'The phone by "{name}" isn\'t found.')
+        return add_contact(args, contacts)
+
 
 @input_error
 def show_phone(name, contacts):
@@ -45,31 +66,36 @@ def show_phone(name, contacts):
     else:
         raise ValueError (f'The phone by "{name}" isn\'t found.')
 
-@input_error
+
 def main():
     contacts = {}
-    print("Welcome to the assistant bot!")
+    
+    print('Welcome to the assistant bot!')
     while True:
-        user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
+        if 'Hello' == input('Enter a command: '):
+            print('How can I help you?')
+            user_input = input('Enter a command: ')
+            command, *args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == "change":
-            print(change_contact(args, contacts))
-        elif command == "phone":
-            print(show_phone(args, contacts))
-        elif command == "all":
-            for name, phone in contacts.items():
-                print(f'{name}: {phone}')
-        else:
-            print("Invalid command.")
+            if command == -1 or command == -2:
+                print('Invalid command')
+            else:
+                if command in ['close', 'exit']:
+                    print('Good bye!')
+                    break
+                elif command == 'hello':
+                    print('How can I help you?')
+                elif command == 'add':
+                    print(add_contact(args, contacts))
+                elif command == 'change':
+                    print(change_contact(args, contacts))
+                elif command == 'phone':
+                    print(show_phone(args, contacts))
+                elif command == 'all':
+                    for name, phone in contacts.items():
+                        print(f'{name}: {phone}')
+                else:
+                    print('This feature will probably be available after updates.')
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
